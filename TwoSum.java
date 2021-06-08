82b8e59d4255cc5be104ebf94bb78f09c79b898a
Sanway Sarkar <32102488+SanwaySarkar@users.noreply.github.com>
2021-06-08 23:06:53 +0530

    Update TwoSum.java

--- TwoSum.java
+++ TwoSum.java
@@ -1,55 +1,14 @@
 import java.util.*;
 class Solution {
-    public int[] twoSum(int[] nums, int target) {
-        int secondNumber =0;int diff=0;
-		int i=0;int j=0;boolean flag = false;
-        HashMap<Integer,Integer> dataMap = new HashMap<>();
-		for( i=0;i<nums.length;i++) {
-			if(dataMap.containsKey(nums[i])){
-				dataMap.put(nums[i], dataMap.get(nums[i])+1);
-			}
-			else
-				dataMap.put(nums[i], 1);
-		}
-        for( i=0;i<nums.length;i++) {
-			diff = target-nums[i];
-			if(dataMap.containsKey(Integer.valueOf(diff)) && diff!=nums[i]) {
-				
-				secondNumber = diff;
-				break;
-			}
-        if(dataMap.containsKey(Integer.valueOf(diff)) && diff==nums[i]) {
-				int count = dataMap.get(diff);
-				secondNumber = diff;
-				if(count>1) {
-					flag = true;
-					break;
-				}
-				else {
-					continue;	
-				}
-		}
+       public int[] twoSum(int[] nums, int target) {
+    Map<Integer, Integer> map = new HashMap<>();
+    for (int i = 0; i < nums.length; i++) {
+        int complement = target - nums[i];
+        if (map.containsKey(complement)) {
+            return new int[] { map.get(complement), i };
         }
-        int temp=0;
-        for(j=0;j<nums.length;j++) {
-			
-			if(secondNumber==nums[j] && (flag)) {
-				if(temp==0) {
-					temp++;
-					continue;
-				}
-				if(temp==1) {
-				break;
-				}
-			}
-			
-			if(secondNumber==nums[j] && (!flag)) {
-				break;
-			}
-		}
-		int res[] = new int[]{i,j};
-        
-        return res;
-        
+        map.put(nums[i], i);
     }
+    throw new IllegalArgumentException("No two sum solution");
+}
 }
